68c60482c416b01e028a76cff75be03232447d3c
Mark Greene <mark.greene@yahoo.com>
2014-01-16 20:04:59 -0500

    Added BYE, BYE, BYE support

--- chapter7.rb
+++ chapter7.rb
@@ -4,22 +4,29 @@ def beer
   while count >= 0
     puts count.to_s + ' bottles of beer on the wall. ' + count.to_s + ' bottles of beer.'
     puts 'Take one down, pass it around.'
-    count = count - 1
+    count -= 1
   end
 end
 
 def grandma
 
+  bye_count = 3
+
   puts 'Talk to grandma'
-  talk = gets.chomp
-  while talk.downcase != 'bye'
-    if talk == talk.upcase
+  while  bye_count > 0
+    talk = gets.chomp
+    if talk == talk.upcase &&  talk != 'BYE'
       puts 'NO, NOT SINCE ' + Random.rand(1930..1950).to_s
     else
+      if talk == 'BYE'
+        bye_count -= 1
+      else
+        bye_count = 3
+      end
+
       puts 'HUH?!  SPEAK UP, SONNY!'
     end
 
-    talk = gets.chomp
   end
 
 end
