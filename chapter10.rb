62d1eea54ff1b6cd4ed9b4c65034ff5fdef9cd30
Mark Greene <mark.greene@yahoo.com>
2014-01-22 21:36:51 -0500

    Added dictionary sort

--- chapter10.rb
+++ chapter10.rb
@@ -22,3 +22,18 @@ def my_recursive_sort unsorted, sorted
   end
   sorted
 end
+
+def my_dictionary_sort unsorted
+
+  sorted = []
+  lower_unsorted = []
+  unsorted.each { |word| lower_unsorted.push word.downcase }
+
+  while !unsorted.empty?
+    index = lower_unsorted.each_with_index.min[1]
+    sorted.push unsorted[index]
+    unsorted.delete_at index
+    lower_unsorted.delete_at index
+  end
+  sorted
+end
--- spec/chapter10_spec.rb
+++ spec/chapter10_spec.rb
@@ -3,13 +3,27 @@ require_relative '../chapter10.rb'
 describe "my sort" do
 
   words = ["Fred", "Bill", "Alvin", "Zack", "Mark", "Bill", "John"]
+  words2 = ["Fred", "bill", "Alvin", "zack", "Mark", "Bill", "john"]
+  sorted_words2 = ["Alvin", "bill", "Bill", "Fred", "john", "Mark", "zack"]
 
   it "should sort an array" do
     expect(my_sort Array.new(words)).to eq Array.new(words).sort
   end
 
+  it "should sort an empty array" do
+    expect(my_sort  []).to eq []
+  end
+
+  it "should sort a one element array" do
+    expect(my_sort ["one"]).to eq ["one"]
+  end
+
   it "should recursively sort an array" do
     expect(my_recursive_sort Array.new(words), []).to eq Array.new(words).sort
+  end
+
+  it "should dictionary sort an array" do
+    expect(my_dictionary_sort Array.new(words2)).to eq sorted_words2
   end
 
 end
