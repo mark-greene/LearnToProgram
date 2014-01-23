145288f336af8a1012436950fad62a29dccc2636
Mark Greene <mark.greene@yahoo.com>
2014-01-23 08:43:44 -0500

    Added English Number

--- chapter10.rb
+++ chapter10.rb
@@ -1,10 +1,4 @@
-
-def push_smallest_and_remove unsorted, sorted
-  smallest = unsorted.each_with_index.min
-  sorted.push smallest[0]
-  unsorted.delete_at smallest[1]
-end
-
+# Sort
 def my_sort unsorted
 
   sorted = []
@@ -23,17 +17,117 @@ def my_recursive_sort unsorted, sorted
   sorted
 end
 
+def push_smallest_and_remove unsorted, sorted
+  smallest = unsorted.each_with_index.min
+  sorted.push smallest[0]
+  unsorted.delete_at smallest[1]
+end
+
+# Dictionary Sort
 def my_dictionary_sort unsorted
 
   sorted = []
-  lower_unsorted = []
-  unsorted.each { |word| lower_unsorted.push word.downcase }
-
   while !unsorted.empty?
-    index = lower_unsorted.each_with_index.min[1]
+    index = find_smallest unsorted
     sorted.push unsorted[index]
     unsorted.delete_at index
-    lower_unsorted.delete_at index
   end
   sorted
 end
+
+def find_smallest unsorted
+
+  smallest = unsorted[0].downcase
+  position = 0
+  unsorted.each_with_index do | value, index |
+    if value.downcase < smallest
+      smallest = unsorted[index].downcase
+      position = index
+    end
+  end
+  position
+end
+
+
+# English Number
+def english_number number
+
+  if number < 0 # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
+  end
+
+  if number == 0
+    return 'zero'
+  end
+
+  number_string = '' # This is the string we will return.
+
+  ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+  tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  teen_place = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  # "left" is how much of the number we still have left to write out.
+  # "write" is the part we are writing out right now.
+  left = number
+
+  number_string, left = decimal_place 1000000000000, 'trillion', number_string, left
+  number_string, left = decimal_place 1000000000, 'billion', number_string, left
+  number_string, left = decimal_place 1000000, 'million', number_string, left
+  number_string, left = decimal_place 1000, 'thousand', number_string, left
+  number_string, left = decimal_place 100, 'hundred', number_string, left
+
+  write = left/10 # How many tens left?
+  left = left - write*10 # Subtract off those tens.
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      # Since we can't write "tenty-two" instead of
+      # "twelve", we have to make a special exception
+      # for these.
+      number_string = number_string + teen_place[left-1]
+      # The "-1" is because teen_place[3] is
+      # 'fourteen', not 'thirteen'.
+      # Since we took care of the digit in the
+      # ones place already, we have nothing left to write.
+      left = 0
+    else
+      number_string = number_string + tens_place[write-1]
+      # The "-1" is because tens_place[3] is
+      # 'forty', not 'thirty'.
+    end
+
+    if left > 0
+      # So we don't write 'sixtyfour'...
+      number_string = number_string + '-'
+    end
+  end
+
+  write = left # How many ones left to write out?
+  left = 0 # Subtract off those ones.
+
+  if write > 0
+    number_string = number_string + ones_place[write-1]
+    # The "-1" is because ones_place[3] is
+    # 'four', not 'three'.
+  end
+
+  # Now we just return "number_string"...
+  number_string
+end
+
+def  decimal_place number, name, number_string, left
+
+  write = left/number
+  left = left - write*number
+  if write > 0
+    decimal = english_number write
+    number_string = number_string + decimal + ' ' +name
+    if left > 0
+      number_string = number_string + ' '
+    end
+  end
+  return number_string, left
+end
+
--- spec/chapter10_spec.rb
+++ spec/chapter10_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../chapter10.rb'
 
-describe "my sort" do
+describe "my_sort" do
 
   words = ["Fred", "Bill", "Alvin", "Zack", "Mark", "Bill", "John"]
   words2 = ["Fred", "bill", "Alvin", "zack", "Mark", "Bill", "john"]
@@ -24,6 +24,59 @@ describe "my sort" do
 
   it "should dictionary sort an array" do
     expect(my_dictionary_sort Array.new(words2)).to eq sorted_words2
+  end
+
+end
+
+describe "english_number" do
+
+  it "should convert to zero" do
+    expect(english_number(0)).to eq "zero"
+  end
+  it "should convert to nine" do
+    expect(english_number(9)).to eq "nine"
+  end
+  it "should convert to ten" do
+    expect(english_number(10)).to eq "ten"
+  end
+  it "should convert to eleven" do
+    expect(english_number(11)).to eq "eleven"
+  end
+  it "should convert to seventeen" do
+    expect(english_number(17)).to eq "seventeen"
+  end
+  it "should convert to thirty-two" do
+    expect(english_number(32)).to eq "thirty-two"
+  end
+  it "should convert to eighty-eight" do
+    expect(english_number(88)).to eq "eighty-eight"
+  end
+  it "should convert to ninety-nine" do
+    expect(english_number(99)).to eq "ninety-nine"
+  end
+  it "should convert to one hundred" do
+    expect(english_number(100)).to eq "one hundred"
+  end
+  it "should convert to one hundred one" do
+    expect(english_number(101)).to eq "one hundred one"
+  end
+  it "should convert to two hundred thirty-four" do
+    expect(english_number(234)).to eq "two hundred thirty-four"
+  end
+  it "should convert to three thousand two hundred eleven" do
+    expect(english_number(3211)).to eq "three thousand two hundred eleven"
+  end
+  it "should convert to nine hundred ninety-nine thousand nine hundred ninety-nine" do
+    expect(english_number(999999)).to eq "nine hundred ninety-nine thousand nine hundred ninety-nine"
+  end
+  it "should convert to one million" do
+    expect(english_number(1000000)).to eq "one million"
+  end
+  it "should convert to one billion" do
+    expect(english_number(1000000000)).to eq "one billion"
+  end
+  it "should convert to one trillion" do
+    expect(english_number(1000000000000)).to eq "one trillion"
   end
 
 end
