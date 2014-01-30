c74d1cf3f1f6abd84cf293d7a5fad212950d11a9
Mark Greene <mark.greene@perceptivesoftware.com>
2014-01-30 14:55:37 -0500

    working on a new version

---
+++ blackjack2.rb
@@ -0,0 +1,256 @@
+# blackjack2.rb
+
+class Card
+  SUITS = [:Hearts, :Spades, :Diamonds, :Clubs]
+  RANKS = [*2..10, :Jack, :Queen, :King, :Ace]
+
+  def initialize rank, suit
+    if RANKS.include?(rank) && SUITS.include?(suit)
+      @card = [rank, suit]
+    else
+      raise ArgumentError, "invalud :rank or :suit"
+    end
+  end
+
+  def to_s
+    rank, suit = @card
+    "#{rank} of #{suit}"
+  end
+
+  def value
+    rank, suit = @card
+
+    case rank
+    when :Ace
+      11
+    when :King
+      10
+    when :Queen
+      10
+    when :Jack
+      10
+    else
+      rank.to_i
+    end
+  end
+end
+
+class Deck
+  attr_accessor :number_of_decks
+
+  def initialize number_of_decks
+    @number_of_decks = number_of_decks
+    @cards = []
+    for i in 1..number_of_decks
+      Card::SUITS.product( Card::RANKS ) { | suit, rank | @cards << Card.new(rank, suit) }
+    end
+  end
+
+  def print
+    puts "#{@cards.count}-card deck"
+    @cards.each do | card |
+      puts "  #{card}"
+    end
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def cut
+    @cards.rotate! @cards.count / 2
+  end
+
+  def draw
+    @cards.shift
+  end
+end
+
+
+SUITS = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+RANKS = [*2..10, 'Jack', 'Queen', 'King', 'Ace']
+CARDS = []
+  SUITS.product( RANKS ) { | suit, rank | CARDS << [rank, suit] }
+
+  def print_cards cards
+    puts "#{cards.count}-card deck"
+    cards.each do | rank, suit |
+      puts "  #{rank} of #{suit}"
+    end
+  end
+
+  def shuffle_cards cards
+    cards.shuffle!
+  end
+
+  def cut_cards cards
+    cards.rotate! cards.count / 2
+  end
+
+  def cards_contain cards, card
+    cards.each do | rank, suit |
+      if rank == card
+        return true
+      end
+    end
+    false
+  end
+
+  def print_card card
+    rank, suit = card
+    puts "#{rank} of #{suit}"
+  end
+
+  def draw_card cards
+    cards.shift
+  end
+
+  def value_of_card card
+    rank, suit = card
+
+    case rank
+    when 'Ace'
+      11
+    when 'King'
+      10
+    when 'Queen'
+      10
+    when 'Jack'
+      10
+    else
+      rank.to_i
+    end
+  end
+
+  def value_of_hand hand
+    ace_count = 0
+    value = 0
+
+    hand.each do | card |
+      rank, suit = card
+      value += value_of_card card
+      if rank == 'Ace'
+        ace_count += 1
+      end
+    end
+
+    while value > 21 && ace_count > 0
+      value -= 10
+      ace_count -= 1
+    end
+    value
+  end
+
+  def player_strategy hand, dealer_up_card
+    strategy = :stand
+    hand_value = value_of_hand hand
+    card_value = value_of_card dealer_up_card
+
+    if hand.count == 2 && cards_contain(hand, 'Ace')
+      if hand_value >= 19
+        strategy =  :stand
+      elsif hand_value == 18 && [2, 7, 8].include?(card_value)
+        strategy =  :stand
+      else
+        strategy = :hit
+      end
+    else
+      if hand_value <= 11 || (hand_value == 12 && card_value.between?(2, 3))
+        strategy =  :hit
+      elsif hand_value >= 17 || card_value.between?(2, 6)
+        strategy =  :stand
+      elsif hand_value < (card_value + 10)
+        strategy =  :hit
+      end
+    end
+    strategy
+  end
+
+  def dealer_strategy hand
+    v = value_of_hand hand
+    v >= 17 && :stand || :hit
+  end
+
+  def results_of_hand hand
+    v = value_of_hand hand
+    case v
+    when 21
+      hand.count == 2 && :blackjack || 21
+    when 12..20
+      v
+    when 0..11
+      raise "error, illegal hand"
+    else
+      :bust
+    end
+  end
+
+  def load_shoe number_of_decks
+    shoe = []
+    for i in 1..number_of_decks
+      shoe += CARDS
+    end
+    shoe
+  end
+
+
+def game_simulation number_of_decks = 6, percent_reserved = 25.0
+
+  cards = load_shoe number_of_decks
+
+  number_of_reserve_cards = (cards.count.to_f * percent_reserved / 100).to_i
+#  puts "Playing blackjack with #{number_of_decks} decks (#{cards.count} cards)" +
+#      " and #{percent_reserved}% (#{number_of_reserve_cards} cards) in reserve"
+
+  dealer_wins = 0
+  player_wins = 0
+  pushes = 0
+
+  cards = shuffle_cards cards
+  cards = cut_cards cards
+
+  while cards.count > number_of_reserve_cards
+
+    player = []
+    dealer = []
+    for i in 1..2
+      card = draw_card cards
+      player += [card]
+      card = draw_card cards
+      dealer += [card]
+    end
+
+    while player_strategy(player, dealer[1]) == :hit
+      card = draw_card cards
+      player += [card]
+    end
+
+    while dealer_strategy(dealer) == :hit
+      card = draw_card cards
+      dealer += [card]
+    end
+
+    player_result = results_of_hand(player)
+    dealer_result = results_of_hand(dealer)
+    if  (dealer_result == :blackjack && player_result != :blackjack) || player_result == :bust
+  #    puts "Dealer wins with #{dealer_result}"
+      dealer_wins += 1
+    elsif player_result == :blackjack || dealer_result == :bust
+  #    puts "Player wins with #{player_result}"
+      player_wins += 1
+    elsif dealer_result > player_result
+  #    puts "Dealer wins with #{dealer_result}"
+      dealer_wins += 1
+    elsif player_result > dealer_result
+  #    puts "Player wins with #{player_result}"
+      player_wins += 1
+    else
+  #    puts "Push with #{dealer_result}"
+      pushes += 1
+    end
+  # print_cards player
+  # print_cards dealer
+  end
+
+  return dealer_wins, player_wins, pushes
+end
